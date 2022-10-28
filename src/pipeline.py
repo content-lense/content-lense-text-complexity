8a7a19f7859b15353bba542d703b50a746108507
Markus Hörmann <markus@focal-analytics.com>
2022-10-28 11:47:27 +0200

    added docker-compose, moved to /src and fixed reading time

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,22 +1,23 @@
 import textstat
 import spacy
 import textdescriptives as td
+# todo: add english support: if language == "de" else "en_core_web_trf"
+nlp = spacy.load("de_dep_news_trf")
+nlp.add_pipe("textdescriptives")
 
 # str = "de" | "en"
 def calc_statistics(txt: str, language: str = "en"):
     textstat.set_lang(language)
     wienerSachtextIndex = textstat.wiener_sachtextformel(txt, 1)
-    readingTimeInMin = textstat.reading_time(txt, ms_per_char=14.69)
     totalSyllables = textstat.syllable_count(txt)
     totalWords = textstat.lexicon_count(txt, removepunct=True)
+    readingTimeInMin = totalWords/200
     totalSentences = textstat.sentence_count(txt)
     totalChars = textstat.char_count(txt, ignore_spaces=True)
     totalLetters = textstat.letter_count(txt, ignore_spaces=True)
     totalWordsLongerThanThreeSyllables = textstat.polysyllabcount(txt)
     totalSingleSyllableWords = textstat.monosyllabcount(txt)
-
-    nlp = spacy.load("de_dep_news_trf" if language == "de" else "en_core_web_trf")
-    nlp.add_pipe("textdescriptives")
+    
     doc = nlp(txt)
     stats_dict_counts = doc._.counts
     totalUniqueWords = stats_dict_counts["n_unique_tokens"]
@@ -44,8 +45,7 @@ def calc_statistics(txt: str, language: str = "en"):
             "medianCharsPerWord": medianCharsPerWord,
             "totalWordsLongerThanThreeSyllables": totalWordsLongerThanThreeSyllables,
             "totalSingleSyllableWords": totalSingleSyllableWords,
-            "totalUniqueWords": totalUniqueWords,
-            "averageWordsPerSentence": totalWords/totalSentences
+            "totalUniqueWords": totalUniqueWords
         }
     }
     return res_dict
@@ -61,7 +61,7 @@ def processArticle(article):
 
     try:
         for part in article:
-            if (part in ["heading", "summary", "body"] & type(article[part]) == str ):
+            if ((part in ["heading", "summary", "body"]) and (type(article[part]) == str )):
                 print("calculate %s" % (part))
                 part_json = calc_statistics(article[part])
                 res[part] = part_json
